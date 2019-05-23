aedc4e6e26614aa769e3137889a671fcf2adb080
zhian154 <1546720585@qq.com>
2019-05-23 22:44:17 +0800

    sdhf

---
+++ src/com/neuedu/test/homework.java
@@ -0,0 +1,13 @@
+package com.neuedu.test;
+
+public class homework {
+    public static void main(String[] args) {
+        int a=153;
+        int m=0;
+        while(a!=0){
+            m=a%10;
+            System.out.print(m);
+            a=a/10;
+        }
+    }
+}
