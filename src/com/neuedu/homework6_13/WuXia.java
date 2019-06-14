2e3ad1aaa956ca78170087fb76aab1d08197195a
zhian154 <1546720585@qq.com>
2019-06-14 08:10:02 +0800

    mm

--- src/com/neuedu/homework6_13/WuXia.java
+++ src/com/neuedu/homework6_13/WuXia.java
@@ -9,14 +9,18 @@ public class WuXia {
         map.put(2, "周芷若");
         map.put(3, "汪峰");
         map.put(4, "灭绝师太");
+        System.out.println("循环");
         for(Map.Entry<Integer,String> entry:map.entrySet()){
             System.out.println(entry.getKey()+ " "+ entry.getValue());
         }
+        System.out.println("插入");
         map.put(5, "李晓红");
+        System.out.println("删除");
         map.remove(1);
         for(Map.Entry<Integer,String> entry:map.entrySet()){
             System.out.println(entry.getKey()+ " "+ entry.getValue());
         }
+        System.out.println("修改");
         map.put(2,"周林");
         for(Map.Entry<Integer,String> entry:map.entrySet()){
             System.out.println(entry.getKey()+ " "+ entry.getValue());
